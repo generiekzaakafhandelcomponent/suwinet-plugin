555c4b0e4a1226e1bcb52c5a9a34ef2fedd3e973
haiko van der schaaf <hvdschaaf@gmail.com>
2025-10-20 13:01:48 +0200

    config plugin service task

--- frontend/projects/plugin/src/public_api.ts
+++ frontend/projects/plugin/src/public_api.ts
@@ -34,4 +34,5 @@ export * from './lib/components/kadasterobjecten/kadastrale-objecten.component'
 export * from './lib/components/rdw-voertuigen/rdw-voertuigen.component';
 export * from './lib/components/svb-persoons-info/svb-persoons-info.component';
 export * from './lib/components/uwv-inkomsten-persoon-info/uwv-inkomsten-info.component';
+export * from './lib/components/bijstandsregelingen-info/bijstandsregelingen-info.component'
 
